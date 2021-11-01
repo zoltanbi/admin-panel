3ef318c341159ed5be1f646090b5e3572e01ab97
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-31 21:56:19 -0700

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './assets/boxicons-2.0.9/css/boxicons.min.css';
+import './assets/css/grid.css';
+import './assets/css/index.css';
+
+import Layout from './components/layout/Layout';
+
+document.title="Zoltan's Admin Panel"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout/>
   </React.StrictMode>,
   document.getElementById('root')
 );
