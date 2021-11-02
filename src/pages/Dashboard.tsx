8165d5c84694b29ccb325559eede780d6e8a9dc7
Zoltanbi <zoltanbiro001@gmail.com>
2021-11-01 20:25:46 -0700

    make dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
+import StatusCard from '../components/status-card/StatusCard'
+
+import statusCards from '../assets/JsonData/status-card-data.json'
+
 const Dashboard = () => {
     return (
         <div>
-            Dashboard
+            <h2 className='page-header'>Dashboard</h2>
+            <div className="row">
+                <div className="col-6">
+                    <div className="row">
+                        {
+                            statusCards.map((item, index) => (
+                                <div className="col-6">
+                                    {/* Status card here */}
+                                    {item.title}
+                                    <StatusCard
+                                        icon={item.icon}
+                                        count={item.count}
+                                        title={item.title}
+                                    />
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
